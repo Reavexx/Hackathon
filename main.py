b1a75535d5d27983bc6c1b66facffbc24e760aaa
Dines Nimalthas <dines_nimalthas@sluz.ch>
2022-10-28 22:06:19 +0200

    Vol 3.9

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 # To get you started we've included code to prevent your Battlesnake from moving backwards.
 # For more info see docs.battlesnake.com
 
-from multiprocessing.reduction import duplicate
+# from multiprocessing.reduction import duplicate
 import random
 import typing
 
