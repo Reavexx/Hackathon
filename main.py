62a19a66ec32b8c2d1226d18ad3a1d4efef44c5d
Dines Nimalthas <dines_nimalthas@sluz.ch>
2022-10-29 11:58:14 +0200

    2

--- main.py
+++ main.py
@@ -133,7 +133,6 @@ def move(game_state: typing.Dict) -> typing.Dict:
     op_next_move = []
     for op in snakes[1:]:
         Op_head = op['head']
-        print(Op_head)
         op_next_move_left = [Op_head["x"] - 1, Op_head["y"]]
         op_next_move_right = [Op_head["x"] + 1, Op_head["y"]]
         op_next_move_down = [Op_head["x"], Op_head["y"] - 1]
@@ -142,7 +141,12 @@ def move(game_state: typing.Dict) -> typing.Dict:
         op_next_move.append(op_next_move_right)
         op_next_move.append(op_next_move_down)
         op_next_move.append(op_next_move_up)
-
+    print(op_next_move)
+    print(op_next_move_left)
+    print(op_next_move_right)
+    print(op_next_move_down)
+    print(op_next_move_up)
+ 
     for Op_move in op_next_move:
         if Op_move == next_move_left:
             is_move_safe["left"] = False
