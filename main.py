f50c4c5b2e2c07174d89747b770f31dc2563cf5d
Dines Nimalthas <dines_nimalthas@sluz.ch>
2022-10-29 02:26:29 +0200

    ......

--- main.py
+++ main.py
@@ -85,21 +85,21 @@ def move(game_state: typing.Dict) -> typing.Dict:
         
 
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+    #TODO: Step 2 - Prevent your Battlesnake from colliding with itself
+    my_body = game_state['you']['body']
     
-    # for bodypart in my_body:
-    #     if my_head["x"] +1 == bodypart["x"] and my_head["y"] == bodypart["y"]: 
-    #         is_move_safe["right"] = False
+    for bodypart in my_body:
+        if my_head["x"] +1 == bodypart["x"] and my_head["y"] == bodypart["y"]: 
+            is_move_safe["right"] = False
 
-    #     if my_head["x"] -1 == bodypart["x"] and my_head["y"] == bodypart["y"]: 
-    #         is_move_safe["left"] = False
+        if my_head["x"] -1 == bodypart["x"] and my_head["y"] == bodypart["y"]: 
+            is_move_safe["left"] = False
 
-    #     if my_head["x"] == bodypart["x"] and my_head["y"] -1 == bodypart["y"]: 
-    #         is_move_safe["down"] = False
+        if my_head["x"] == bodypart["x"] and my_head["y"] -1 == bodypart["y"]: 
+            is_move_safe["down"] = False
 
-    #     if my_head["x"] == bodypart["x"] and my_head["y"] +1 == bodypart["y"]: 
-    #         is_move_safe["up"] = False
+        if my_head["x"] == bodypart["x"] and my_head["y"] +1 == bodypart["y"]: 
+            is_move_safe["up"] = False
 
     # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     snakes = game_state['board']['snakes']
