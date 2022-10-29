e955affe525d804b8ca3d8343470699c663ff039
Dines Nimalthas <dines_nimalthas@sluz.ch>
2022-10-29 12:03:54 +0200

    hallo

--- main.py
+++ main.py
@@ -131,16 +131,17 @@ def move(game_state: typing.Dict) -> typing.Dict:
 
 # TODO: Step 5 - Avoid Opponents next move
     op_next_move = []
-    for op in snakes[2:]:
-        Op_head = op['head']
-        op_next_move_left = [Op_head["x"] - 1, Op_head["y"]]
-        op_next_move_right = [Op_head["x"] + 1, Op_head["y"]]
-        op_next_move_down = [Op_head["x"], Op_head["y"] - 1]
-        op_next_move_up = [Op_head["x"], Op_head["y"] + 1]
-        op_next_move.append(op_next_move_left)
-        op_next_move.append(op_next_move_right)
-        op_next_move.append(op_next_move_down)
-        op_next_move.append(op_next_move_up)
+    for op in snakes:
+        if op["id"] != game_state["you"]["id"]:
+            Op_head = op['head']
+            op_next_move_left = [Op_head["x"] - 1, Op_head["y"]]
+            op_next_move_right = [Op_head["x"] + 1, Op_head["y"]]
+            op_next_move_down = [Op_head["x"], Op_head["y"] - 1]
+            op_next_move_up = [Op_head["x"], Op_head["y"] + 1]
+            op_next_move.append(op_next_move_left)
+            op_next_move.append(op_next_move_right)
+            op_next_move.append(op_next_move_down)
+            op_next_move.append(op_next_move_up)
     print(op_next_move)
     print(op_next_move_left)
     print(op_next_move_right)
